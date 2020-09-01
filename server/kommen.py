c03f821134a466176b9a34ab2aab290f4b355684
Jason M. Pittman <jason@jasonmpittman.com>
2020-08-31 21:17:52 -0400

    updated kommen for fw testing

--- server/kommen.py
+++ server/kommen.py
@@ -21,6 +21,7 @@ from listeners import tcp_server
 #from listeners import udp_server
 from handlers import asym_cryptography_handler as asym_crypto
 from handlers import registration_handler as registration
+from handlers import firewall_handler as firewall
 
 class Kommen:
     def __init__(self, config_file):
@@ -33,7 +34,8 @@ class Kommen:
 
     def run(self):
 
-        c = asym_crypto.AsymmetricCryptographyHandler()
+        # crypto testing
+        #c = asym_crypto.AsymmetricCryptographyHandler()
         #print(c.do_keys_exist()) # test 1
         
         #if not c.do_keys_exist('ClientA'): # test 2
@@ -48,12 +50,19 @@ class Kommen:
         #plain = c.decrypt(cipher, 'ClientA_private.pem') # test 5
         #print(plain)
 
-        obj = 'test'.encode("utf8")
-        signature = c.sign(obj) # test 6
+        #obj = 'test'.encode("utf8")
+        #signature = c.sign(obj) # test 6
 
-        obj2 = 'testing'.encode("utf8")
-        result = c.is_sign_valid(obj2, signature) # test 7
-        print(result)
+        #obj2 = 'testing'.encode("utf8")
+        #result = c.is_sign_valid(obj2, signature) # test 7
+        #print(result)
+
+        # firewall testing
+
+        fw = firewall.FirewallHandler()
+        chains = fw.get_chains()
+        for chain in chains:
+            print(chain)
 
         #server = tcp_server.TcpServer(self.ip_address, self.port, self.max_conn)
         #server.run_server()
